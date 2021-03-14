38b761569064c8d885e8d88da107eeb8fa7db6f7
Matthew <matthew.rebehn@gmail.com>
2021-03-13 21:36:02 -0500

    added different navbar | deleted layout.css and replaced with a css reset

---
+++ src/components/Navigation.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap"
+
+const Navigation = ({ siteTitle }) => (
+  <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">{siteTitle}</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">Features</Nav.Link>
+        <Nav.Link href="#pricing">Pricing</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+        <Button variant="outline-info">Search</Button>
+      </Form>
+    </Navbar>
+  </>
+)
+
+Navigation.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Navigation.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Navigation
--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Navigation from "./Navigation"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Navigation siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
